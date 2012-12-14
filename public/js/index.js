7717391bc1fd6206c6ba402c9913647a75f8f5ee
weiwally <weiwally@weiwally-VT633AA-AB0-p6330tw.(none)>
2012-12-15 00:19:05 +0800

    Adding function Login and Register

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,34 @@
 $(function(){
-console.log('hi');
+var auditspace = Backbone.View.extend({
+	el: "body",
+	events:{
+		"click input#submit" : "log_in",
+		"click input#register" : "register"
+	},
+	log_in:function(event){
+		event.preventDefault();
+		console.log("log_info POST!!");
+		var login_obj={email: $('input#email:first').val(), password: $('input#password:first').val()};
+		console.log(login_obj);
+		$.post('/login',login_obj,function(data){
+			console.log(data);
+		})
+	},
+	register:function(event){
+		event.preventDefault();
+		console.log("Reg_info POST");
+		if($('#reg_pass').val()!=$('#con_pass').val())
+			$('.reg div#error').html('password unmatched!!');
+		else{
+			var reg_obj = {email: $('.reg input#email').val(), password:$('#reg_pass').val() , first_name: $('#first').val(), last_name: $('#last').val(), id: $('#id').val()};
+			console.log(reg_obj);
+			$.post('/signup',reg_obj,function(data){
+				console.log(data);
+			});
+			$('.reg div#error').empty();
+		}
+	}
+});
 var workspace = Backbone.Router.extend({	
     			routes :{
 					'register' : 'register',
@@ -27,6 +56,7 @@ var workspace = Backbone.Router.extend({
 					});
 				}
 });
+new auditspace();
 new workspace();
 Backbone.history.start();
 });
