36e8600ec2b04562314478ba0ee3b77e913fefe3
weiwally <weiwally@weiwally-VT633AA-AB0-p6330tw.(none)>
2012-12-17 10:52:46 +0800

    timeline-bugging

--- public/js/user.js
+++ public/js/user.js
@@ -2,6 +2,16 @@ var eventnum = 0,i,id=2;
 //$("div").qrcode("http://www.google.com.tw");
 var Banvas_id = getCookie('Banvas_id'),
 	Banvas_token = getCookie('Banvas_token');
+var timeline = {
+		"timeline":
+		{   
+				"headline":"Default",
+				"type":"default",
+				"text":"Default",
+				"startDate":"2000,1,1",
+				"date": [{"startDate":"2000,1,1","endDate":"2000,1,30","headline":"Default","text":"<p>Welcome to Banvas</p>","asset":{"media":"","credit":"","caption":""}}] 
+		}   
+};
 $.post('/'+Banvas_id+'/status',{"token": Banvas_token},function(data){
 	console.log(data);
 	var info = JSON.parse(data);
@@ -18,6 +28,18 @@ $.post('/'+Banvas_id+'/status',{"token": Banvas_token},function(data){
 		var img_url=JSON.parse(info.data[0].Image_pkt);
 		$('img.head').attr('src','/uploads/'+img_url.head_url);
 		$('a.FB').attr('href',info.data[0].linked.Facebook);
+		var temp="default";
+	/*	if(temp.match(info.data[0].TimeLine)==null){
+			timeline=JSON.parse(info.data[0].TimeLine);
+		}
+		$('#timeline-embed').empty();
+		createStoryJS({
+			type:       'timeline',
+			width:      '100%',
+			height:     '600',
+			source:     timeline,
+			embed_id:   'timeline-embed',           // ID of the DIV you want to load the timeline into
+		});*/
 	}
 })
 $(".edit").click(edit_mode);
@@ -27,7 +49,7 @@ function edit_mode(){
 			$(this).html("Edit").addClass("edit").unbind('.click');
 			$(".static").unbind('click');
 			$('.temp').remove();
-			var post_data = {"token": Banvas_token,"School": $('.school:first').html(),"Intro":$('.intro').html(),"Skill":$('.skill').html(),"Position":$('.position:first').html(),"linked":{"Facebook":$('a.FB').attr('href'),"Blogger":'#',"Linkedin":'#'}};
+			var post_data = {"token": Banvas_token,"School": $('.school:first').html(),"Intro":$('.intro').html(),"Skill":$('.skill').html(),"Position":$('.position:first').html(),"linked":{"Facebook":$('a.FB').attr('href'),"Blogger":'#',"Linkedin":'#'},"TimeLine":JSON.stringify(timeline)};
 			$.post('/'+Banvas_id+'/modify',post_data,function(data){
 				console.log(data);
 			});
@@ -46,67 +68,9 @@ function edit_mode(){
 		$('<button class="temp">+</button>').appendTo('.skill_header').click(function(){
 			$('<li class="static">default</li>').appendTo('ul.skill').click(edit).change(save).end();
 		});
-		$('<button class="temp" style="float : right;">Add Social Network Link</button>').appendTo('div.social').click(function(){
-			$("<form><label>Facebook:</label><input type='text' id='FB_url'/><br><label>Blog:</label><input type='text' id='Blog_url'/><br><label>LinkedIn:</label><input type='text' id='Linked_url'/></form>").dialog({
-				buttons: {
-					"Set": function(){
-						$('a.FB').attr('href',$('#FB_url').val());
-						$( this ).dialog( "close" );
-					},
-					"Cancel": function(){
-						$( this ).dialog( "close" );
-					}
-				}
-			});
-		});
-};
-$('#scroll-right').on('mouseup mouseleave',function(){
-	window.clearInterval(i);
-}).mousedown(function(){
-	var temp = $('.ui-slider').css('left');
-	i = window.setInterval(function() {$('.ui-slider').css('left','+=10');$('ul.timeline').css('left','+=10');}, 100);
-});
-$('#scroll-left').on('mouseup mouseleave',function(){
-	window.clearInterval(i);
-}).mousedown(function(){
-	var temp = $('.ui-slider').css('left');
-	i = window.setInterval(function() {$('.ui-slider').css('left','-=10');$('ul.timeline').css('left','-=10');}, 100);
-});
-var timeline = {
-		"timeline":
-		{   
-				"headline":"Sh*t People Say",
-				"type":"default",
-				"text":"People say stuff",
-				"startDate":"2012,1,26",
-				"date": [
-				{   
-						"startDate":"2012,1,27",
-						"endDate":"2012,1,28",
-						"headline":"Sh*t Politicians Say",
-						"text":"<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p>",
-						"asset":{
-								"media":"http://www.google.com",
-								"credit":"",
-								"caption":""
-						}   
-				},  
-				{   
-						"startDate":"2012,1,29",
-						"endDate":"2012,1,30",
-						"headline":"Test",
-						"text":"<p>hahaha</p>",
-						"asset":{}
-				},  
-				{   
-						"startDate":"2012,1,17",
-						"endDate":"2012,1,18",
-						"headline":"Meow",
-						"text":"<p>Already 00:12!!time to sleep</p>",
-						"asset":{}
-				}   
-				] 
-		}   
+		$('<button class="temp" style="float : right;">Add Social Network Link</button>').appendTo('div.social').click(Social_url);
+		$('<button class="temp" id="add_time_event">Add Timeline Event</button>').insertAfter('div#timeline').click(AddTimeEvent);
+		$('<button class="temp" id="timeline_config">Timeline Config</button>').insertAfter('div#timeline').click(Timeline_config);
 };
 $('#timeline-embed').ready(function(){
 	createStoryJS({
@@ -115,42 +79,9 @@ $('#timeline-embed').ready(function(){
 		height:     '600',
 		source:     timeline,
 		embed_id:   'timeline-embed',           // ID of the DIV you want to load the timeline into
-	});
+	}); 
 });
-$('button#add_time_event').click(function(){
-	var temp = _.template($('#add_time_form').html(),{});
-	$(temp).dialog({
-		height: 300,
-		width: 400,
-		modal: true,
-		buttons : {
-			"Create": function(){
-				var new_event={
-					"startDate": $('#startDate').val(),
-					"endDate": $('#endDate').val(),
-					"headline":$('#headline').val(),
-					"text": '<p>'+$('#text').val()+'</p>',
-					"asset":{
-						"media":$('#url').val()
-					}
-				}
-				timeline.timeline.date.push(new_event);
-				$('#timeline-embed').empty();
-				createStoryJS({
-					type:       'timeline',
-					width:      '100%',
-					height:     '600',
-					source:     timeline,
-					embed_id:   'timeline-embed',           // ID of the DIV you want to load the timeline into
-				});
-				$( this ).dialog( "close" );
-			},
-			"Cancel": function(){
-				$( this ).dialog( "close" );
-			}
-		}
-	});
-});
+// Function Area
 function save(){
 			$(".editing").each(function(){
 				var temp=$(this).val();
@@ -173,4 +104,83 @@ function getCookie(c_name){
 						return unescape(y);
 				}
 		}
+}
+function Social_url(){
+	$("<form><label>Facebook:</label><input type='text' id='FB_url'/><br><label>Blog:</label><input type='text' id='Blog_url'/><br><label>LinkedIn:</label><input type='text' id='Linked_url'/></form>").dialog({
+		buttons: {
+			"Set": function(){
+				$('a.FB').attr('href',$('#FB_url').val());
+				$( this ).dialog( "close" );
+			},
+			"Cancel": function(){
+				$( this ).dialog( "close" );
+				}
+		}
+	});
+}
+function AddTimeEvent(){
+	var temp = _.template($('#add_time_form').html(),{});
+	$(temp).dialog({
+		height: 300,
+		width: 400,
+		modal: true,
+		buttons : {
+			"Create": function(){
+				var new_event={
+					"startDate": $('#startDate').val(),
+					"endDate": $('#endDate').val(),
+					"headline":$('#headline').val(),
+					"text": '<p>'+$('#text').val()+'</p>',
+					"asset":{
+						"media":$('#url').val(),
+						"credit":" ",
+						"caption":" "
+					}
+				}
+				timeline.timeline.date.push(new_event);
+				$('#timeline-embed').empty();
+				createStoryJS({
+					type:       'timeline',
+					width:      '100%',
+					height:     '600',
+					source:     timeline,
+					embed_id:   'timeline-embed',           // ID of the DIV you want to load the timeline into
+				});
+				$( this ).dialog( "close" );	
+			},
+			"Cancel": function(){
+				$( this ).dialog( "close" );
+			}
+		}
+	});
+}
+function Timeline_config(){
+	var temp = _.template($('#Timeline_config').html(),{});
+	$(temp).dialog({
+		height: 300,
+		width: 400,
+		modal: true,
+		buttons : {
+			"Update": function(){
+				if($('#headname').val()!='')
+					timeline.timeline.headline=$('#headname').val();
+				if($('#time_des').val()!='')
+					timeline.timeline.text=$('#time_des').val();
+				if($('#time_start').val()!='')
+					timeline.timeline.startDate=$('#time_start').val();
+				$('#timeline-embed').empty();
+				createStoryJS({
+					type:       'timeline',
+					width:      '100%',
+					height:     '600',
+					source:     timeline,
+					embed_id:   'timeline-embed',           // ID of the DIV you want to load the timeline into
+				});
+				$( this ).dialog( "close" );
+			},
+			"Cancel": function(){
+				$( this ).dialog( "close" );
+			}
+		}
+	});
 }
